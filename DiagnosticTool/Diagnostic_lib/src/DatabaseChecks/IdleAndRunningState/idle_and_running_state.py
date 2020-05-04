c4219cba8b87c447145ef335c499d9842767be8f
pragyak412 <pragyak412@gmail.com>
2020-05-04 12:44:01 +0530

    with idle and running

---
+++ DiagnosticTool/Diagnostic_lib/src/DatabaseChecks/IdleAndRunningState/idle_and_running_state.py
@@ -0,0 +1,93 @@
+import logging
+import os
+import json
+import subprocess
+from campaign_db_connection import PgConnection
+from db_parameters import DbParameters
+from database_exceptions import DBParameterNotFoundException, DataBaseException
+from db_queries import db_query_for_running_state, db_query_for_idle_state
+
+class IdleRunningState:
+
+    def __init__(self, campaign_host):
+        self.campaign_host = campaign_host
+
+    def get_running_queries(self):
+        try:
+            params_obj = DbParameters()
+            params = params_obj.get_db_parameters()
+        except DBParameterNotFoundException as err:
+            logging.error(err)
+            error_message = dict()
+            error_message["error"] = str(err)
+            return error_message
+        if "error" in params:
+            return params
+        else:
+            db_connection_obj = PgConnection(logging, params)
+            result = {}
+            try:
+                db_query_result = db_connection_obj.get_result_from_db(db_query_for_running_state)
+            except DataBaseException as err:
+                logging.error(err)
+                error_message = dict()
+                error_message["error"] = str(err)
+                return error_message
+            if db_query_result == []:
+                result["Running Queries"] = "No query"
+            else:
+                result_rows = []
+                for row in db_query_result:
+                    r = self.make_result_for_runnning(row)
+                    result_rows.append(r)
+                result["Running Queries"] = result_rows
+        return result
+
+    def get_idle_queries(self):
+        try:
+            params_obj = DbParameters()
+            params = params_obj.get_db_parameters()
+        except DBParameterNotFoundException as err:
+            logging.error(err)
+            error_message = dict()
+            error_message["error"] = str(err)
+            return error_message
+        if "error" in params:
+            return params
+        else:
+            db_connection_obj = PgConnection(logging, params)
+            result = {}
+            try:
+                db_query_result = db_connection_obj.get_result_from_db(db_query_for_idle_state)
+            except DataBaseException as err:
+                logging.error(err)
+                error_message = dict()
+                error_message["error"] = str(err)
+                return error_message
+            if db_query_result == []:
+                result["Idle Queries"] = "No query"
+            else:
+                result_rows = []
+                for row in db_query_result:
+                    r = self.make_result_for_idle(row)
+                    result_rows.append(r)
+                result["Idle Queries"] = result_rows
+        return result
+
+    def make_result_for_running(self, row):
+        result = {}
+        result["running_pid"] = row[0]
+        result["running_duration"] = row[1]
+        result["running_query"] = row[2]
+        return result
+
+    def make_result_for_idle(self, row):
+        result = {}
+        result = {}
+        result["idle_pid"] = row[0]
+        result["idle_duration"] = row[1]
+        result["idle_query"] = row[1]
+        return result
+
+
+    
---
+++ DiagnosticTool/Diagnostic_lib/src/DatabaseChecks/IdleAndRunningState/idle_and_running_state_wrapper.py
@@ -0,0 +1,41 @@
+from idle_and_running_state import IdleRunningState
+import sys
+import json
+
+#function to invoke from salt
+def idle_and_running_state_caller(*argv):
+    if len(argv) != 4:
+        return {"error": "Invalid parameters paased to salt"}
+    instance_name = argv[3]
+    idle_and_running_state_obj = IdleRunningState(instance_name)
+    idle_and_running_state = {}
+    output = idle_and_running_state_obj.get_running_queries()
+    idle_and_running_state["Running Query Result"] = output
+    output = idle_and_running_state_obj.get_idle_queries()
+    idle_and_running_state["Idle Query Result"] = output
+    idle_and_running_state = json.dumps(idle_and_running_state)
+    return idle_and_running_state
+
+'''
+#Whenever want to run standalone script
+def idle_and_running_state_caller():
+    print(sys.argv)
+    args = sys.argv
+    if len(args) != 2:
+        error_message = dict()
+        error_message["error"] = "Campaign Host needed as a parameter"
+        return error_message
+    idle_and_running_state_obj = IdleRunningState(args[1])
+    idle_and_running_state = {}
+    output = idle_and_running_state_obj.get_running_queries()
+    idle_and_running_state["Running Query Result"] = output
+    output = idle_and_running_state_obj.get_idle_queries()
+    idle_and_running_state["Idle Query Result"] = output
+    idle_and_running_state = json.dumps(idle_and_running_state)
+    print("Final result")
+    print(idle_and_running_state)
+    return idle_and_running_state
+
+if __name__ == "__main__":
+    idle_and_running_state_caller()
+'''
