afb5591dd431046d760f781e6be5c0d736d73b90
pragkuma <pragkuma@adobe.com>
2020-05-08 20:15:24 +0530

    with workflow daily service check

---
+++ DiagnosticTool/Diagnostic_lib/src/WorkflowChecks/WorkflowDailyService/workflow_daily_service.py
@@ -0,0 +1,40 @@
+import subprocess
+
+class WorkflowDailyService:
+
+    def __init__(self, campaign_host):
+        self.campaign_host = campaign_host
+
+    def get_workflow_daily_service(self):
+        """
+        Getting daily workflow status using subprocess module
+        """
+        result = {}
+        try:
+            proc1 = subprocess.Popen(["nlserver", "pdump" , "-full"], stdout=subprocess.PIPE)
+            proc2 = subprocess.Popen(["grep", "-A 9", "wfserver"], stdin=proc1.stdout,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits.
+            output, err = proc2.communicate()
+            output, err = str(output), str(err)
+            result = self.make_result(output)
+            return result
+        except subprocess.CalledProcessError as err:
+            logging.error(err)
+            error_message = dict()
+            error_message["error"] = str(err)
+            return error_message
+        
+    def make_result(self, output):
+        """
+        Making result to return
+        """
+        result = {}
+        lines = output.split("\n")
+        for line in lines:
+            res = line.split("Start date")
+            if len(res)>1 and int(res[1].split('(')[1].split('h')[0])<48: #to check if workflow ran in last 48 hours
+                result['Daily Workflow Status'] = "Last Run " + str(res[1].split('(')[1].split(')')[0])
+                return result
+        result['Daily Workflow Status'] = "Workflow didn't start"
+        return result
---
+++ DiagnosticTool/Diagnostic_lib/src/WorkflowChecks/WorkflowDailyService/workflow_daily_service_wrapper.py
@@ -0,0 +1,37 @@
+from workflow_daily_service import WorkflowDailyService
+import sys
+import json
+
+#function to invoke from salt
+def workflow_daily_service_caller(*argv):
+    if len(argv) != 4:
+        return {"error": "Invalid parameters paased to salt"}
+    instance_name = argv[3]
+    workflow_daily_service_obj = WorkflowDailyService(instance_name)
+    workflow_daily_service = {}
+    output = workflow_daily_service_obj.get_workflow_daily_service()
+    workflow_daily_service["Workflow Daily Service"] = output
+    workflow_daily_service = json.dumps(workflow_daily_service)
+    return workflow_daily_service
+
+'''
+#Whenever want to run standalone script
+def workflow_daily_service_caller():
+    print(sys.argv)
+    args = sys.argv
+    if len(args) != 2:
+        error_message = dict()
+        error_message["error"] = "Campaign Host needed as a parameter"
+        return error_message
+    workflow_daily_service_obj = WorkflowDailyService(args[1])
+    workflow_daily_service = {}
+    output = workflow_daily_service_obj.get_workflow_daily_service()
+    workflow_daily_service["Workflow Daily Service"] = output
+    workflow_daily_service = json.dumps(workflow_daily_service)
+    print("Final result")
+    print(workflow_daily_service)
+    return workflow_daily_service
+
+if __name__ == "__main__":
+    workflow_daily_service_caller()
+'''
