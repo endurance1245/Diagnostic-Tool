5b0aa22fe016bbeb63aec69d46ddc810e8398438
psaraf <psaraf@adobe.com>
2020-06-24 16:09:05 +0530

    separate single and multiple workflow dag

--- DiagnosticTool/dags/performance_wokflow_multiple.py
+++ DiagnosticTool/dags/performance_wokflow_multiple.py
@@ -15,9 +15,9 @@ args = {
 }
 
 #dags
-performance_workflow_single_dag = DAG(
-    dag_id = 'performance_workflow_single',
-    description = 'If there is performance issue due to single workflow. To trigger the dag, use the trigger_dag REST API only.',
+performance_workflow_multiple_dag = DAG(
+    dag_id = 'performance_workflow_multiple',
+    description = 'Performance issue due to multiple workflows. To trigger the dag, use the trigger_dag REST API only.',
     default_args = args,
     schedule_interval = None,
 )
@@ -30,7 +30,7 @@ opr_check_memory_usage = SaltOperator(
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
 opr_check_swap_usage = SaltOperator(
@@ -40,7 +40,7 @@ opr_check_swap_usage = SaltOperator(
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
 opr_check_mta = SaltOperator(
@@ -50,7 +50,7 @@ opr_check_mta = SaltOperator(
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
 opr_check_cpu = SaltOperator(
@@ -60,7 +60,7 @@ opr_check_cpu = SaltOperator(
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
 opr_count_pid = SaltOperator(
@@ -70,46 +70,24 @@ opr_count_pid = SaltOperator(
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
-opr_workflow_type = SaltOperator(
-    task_id='check_workflow_type',
-    module_name='workflow_type_wrapper.workflow_type_caller',
+#check how many workflows are running on the system
+opr_count_workflow = SaltOperator(
+    task_id='count_workflow',
+    module_name='workflow_count_and_state_wrapper.workflow_count_and_state_caller',
     provide_context = True,
     execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
     retries = DEFAULT_RETRIES,
     retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
+    dag = performance_workflow_multiple_dag,
 )
 
-#Check workflow status if internal is given
-opr_workflow_status = SaltOperator(
-    task_id='check_workflow_satus',
-    module_name='workflow_status_wrapper.workflow_status_caller', #to invoke the function of workflow status
-    provide_context = True,
-    execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
-    retries = DEFAULT_RETRIES,
-    retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
-)
-
-#Check dailyworkflow status if internal is given
-opr_workflow_daily_service = SaltOperator(
-    task_id='check_workflow_daily_service',
-    module_name='workflow_daily_service_wrapper.workflow_daily_service_caller', #to invoke the function of workflow status
-    provide_context = True,
-    execution_timeout = DEFAULT_EXECUTION_TIMEOUT,
-    retries = DEFAULT_RETRIES,
-    retry_delay = DEFAULT_RETRY_DELAY,
-    dag = performance_workflow_single_dag,
-)
 #dependencies
 opr_check_memory_usage
 opr_check_swap_usage
 opr_check_mta
 opr_check_cpu
 opr_count_pid
-opr_workflow_type
-opr_workflow_status
-opr_workflow_daily_service
+opr_count_workflow
