4f198877d043aea0eed84af68c077dcd5a9b9c18
pragyak412 <pragyak412@gmail.com>
2020-06-25 12:40:55 +0530

    change in flask server

--- DiagnosticTool/ui/server.py
+++ DiagnosticTool/ui/server.py
@@ -1,12 +1,12 @@
 from flask_cors import CORS
 from blueprint.apis import dag_blueprint
-from blueprint.test_html import blueprint_html
+from blueprint.routes import sso as sso_blueprint
 import flask
+
 app = flask.Flask(__name__)
 app.register_blueprint(dag_blueprint)
-app.register_blueprint(blueprint_html)
+app.register_blueprint(sso_blueprint)
 app.secret_key = "diagnostictoolcapmaign12345677654321"
-
 CORS(app)
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
