9248e8b75d5810fe6e89cea7585ae7b2ea82f974
psaraf <psaraf@adobe.com>
2020-06-25 16:01:38 +0530

    changes dag retry

--- DiagnosticTool/dags/constants.py
+++ DiagnosticTool/dags/constants.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
 DEFAULT_EXECUTION_TIMEOUT = timedelta(minutes=2)
-DEFAULT_RETRIES =0
+DEFAULT_RETRIES = 0
 DEFAULT_RETRY_DELAY = timedelta(minutes=2)
 DEFAULT_START_DATE = datetime.today() - timedelta(minutes = 0.5)
