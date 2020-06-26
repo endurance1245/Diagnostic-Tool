f04a36ded286a38a417c4a13b6e24d60edec7a80
pragkuma <pragkuma@adobe.com>
2020-06-26 17:30:11 +0530

    change due to neolane user

--- DiagnosticTool/Diagnostic_lib/src/WorkflowChecks/health_check.py
+++ DiagnosticTool/Diagnostic_lib/src/WorkflowChecks/health_check.py
@@ -1,3 +1,8 @@
+activate_this = "/root/diagtool/bin/activate_this.py"
+with open(activate_this) as f:
+        code = compile(f.read(), activate_this, 'exec')
+        exec(code, dict(__file__=activate_this))
+        
 import os
 import psutil
 import subprocess
