2f78e3cc6676c6c322887d773b0c3d937b4d2350
sar <sar@adobe.com>
2020-06-26 14:34:37 +0530

    Update test_timestamp.py

--- DiagnosticTool/Diagnostic_lib/src/salt_modules/test_timestamp.py
+++ DiagnosticTool/Diagnostic_lib/src/salt_modules/test_timestamp.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+'''
 activate_this = "/root/diagtool/bin/activate_this.py"
 with open(activate_this) as f:
     code = compile(f.read(), activate_this, 'exec')
     exec (code, dict(__file__=activate_this))
-
+'''
 import boto3
 import os
 from boto3 import client
