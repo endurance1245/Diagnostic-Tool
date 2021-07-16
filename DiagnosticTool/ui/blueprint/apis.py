651b005543997cb37667e86c5244c500c537964d
SARVESH <sarvesh.@SARVESHs-MacBook-Pro.local>
2021-07-16 15:46:27 +0530

    Diagnostic tooling

--- DiagnosticTool/ui/blueprint/apis.py
+++ DiagnosticTool/ui/blueprint/apis.py
@@ -48,7 +48,8 @@ def setTrigger(taskId):
         execution_date = time
         URL = 'http://10.0.0.183:8080/api/experimental/dags/'+taskId+'/dag_runs'
         try:
-            result = requests.post(URL, headers = {'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}, data = json.dumps({'run_id' : run_id,'execution_date': execution_date, 'conf': conf}))
+            result = requests.post(URL, headers = {'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded'}, 
+            data = json.dumps({'run_id' : run_id,'execution_date': execution_date, 'conf': conf}))
             resultValue = result.json()
             return make_response(resultValue, result.status_code)
         except Exception as er:
